daa86ba9c8fd3f733af8472a9b3f4763aaf25565
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-11-07 16:34:25 -0600

    this

---
+++ 06_this/app.js
@@ -0,0 +1,14 @@
+var Bucky = {
+  printFirstName: function(){
+    console.log("My name is Bucky");
+    console.log(this === Bucky); //true
+  }
+};
+Bucky.printFirstName();
+
+//The default calling context is global
+function doSomethingWorthless() {
+  console.log("\nMake it fresh");
+  console.log(this === global);
+}
+doSomethingWorthless();
