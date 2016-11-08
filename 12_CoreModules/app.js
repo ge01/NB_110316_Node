390cef681fe293932726ce687a1406e3cb7a6393
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-11-08 15:26:17 -0600

    Core Modules

---
+++ 12_CoreModules/app.js
@@ -0,0 +1,29 @@
+console.log(__dirname);
+console.log(__dirname);
+/*
+// This interval runs repeatedly
+setInterval(function (){
+  console.log("beef");
+}, 2000);
+*/
+/*
+// What path can do is normalize the path for different operating systems
+var path = require('path');
+var websiteHome = "Desktop/Bucky//thenewboston/index.html";
+var websiteAbout = "Desktop/Bucky/thenewboston/about.html";
+
+console.log(path.normalize(websiteHome));
+console.log(path.dirname(websiteHome));
+console.log(path.basename(websiteHome));
+console.log(path.extname(websiteHome));
+*/
+/*
+// It's common practice to name the variable the same as the module
+// file system module
+// be able to real and write to files
+var fs = require('fs');
+
+// requires a path, data, options
+fs.writeFileSync("corn.txt", "Corn is good, corn is life");
+console.log(fs.readFileSync("corn.txt").toString());
+*/
