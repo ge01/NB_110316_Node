7c3d32adb5f5ff4aa54aec63badb053a1ebdd7e7
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-11-08 04:25:26 -0600

    9 More on Modules

---
+++ 09_MoreOnModules/app.js
@@ -0,0 +1,6 @@
+// Look at the directory that I am at for a file named movies to import
+// Never include the .js extension
+var movies = require('./movies');
+//movies.avatar();
+movies.printAvatar();
+console.log(movies.favMovie);
---
+++ 09_MoreOnModules/movies.js
@@ -0,0 +1,23 @@
+module.exports = {
+  printAvatar: function(){
+    console.log("Avatar");
+  },
+  printChappie: function(){
+    console.log("Chappie");
+  },
+  favMovie: "The Matrix"  
+};
+
+/*
+function printAvatar(){
+  console.log("Avatar: PG-13");
+}
+
+function printChappie(){
+  console.log("Chappie: R");
+}
+
+// export function printAvatar
+module.exports.avatar = printAvatar;
+module.exports.avatar = printAvatar;
+*/
