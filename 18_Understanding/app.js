e20e6cb021256a32438be157a382e8f88d615eae
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-11-10 15:24:18 -0600

    18 Understanding app.js

---
+++ 18_Understanding/app.js
@@ -0,0 +1,49 @@
+var express = require('express');
+// working with file paths
+var path = require('path');
+// used for logging out info on the terminal
+var logger = require('morgan');
+// for handling cookies
+var cookieParser = require('cookie-parser');
+// when ever parsing a body
+var bodyParser = require('body-parser');
+// the brains of everything
+var index = require('./routes/index');
+
+// intializing the use of express
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
